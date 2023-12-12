e61d71b8011181efc5b5c68f3d8904b59cf281d4
LTgrape <0522jisu2@gmail.com>
2023-12-12 10:13:59 +0900

    1차 세팅

---
+++ src/main/java/com/example/jumak/JumakApplication.java
@@ -0,0 +1,13 @@
+package com.example.jumak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JumakApplication.class, args);
+    }
+
+}
