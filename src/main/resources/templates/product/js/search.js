56eca70c3291144f129bb4257e95e01d2b8e7075
limseol <ycmatic@gmail.com>
2023-12-14 13:15:00 +0900

    setting

---
+++ src/main/resources/templates/product/js/search.js
@@ -0,0 +1,57 @@
+$('.menu1').on("mouseover",function() {
+$('.lnb1').css("display", "block").css("background-color","rgba(0, 0, 255, 0.3)"); 
+});
+$('.lnb1').on("mouseover",function() {
+$('.lnb1').css("display","block")});
+$('.lnb1').on("mouseout",function() {
+$('.lnb1').css("display","none")});
+$('.menu1').on("mouseout",function() {
+$('.lnb1').css("display","none")});
+
+
+$('.menu2').on("mouseover",function() {
+$('.lnb2').css("display", "block").css("background-color","rgba(0, 0, 255, 0.3)"); 
+});
+$('.lnb2').on("mouseover",function() {
+$('.lnb2').css("display","block")});
+$('.lnb2').on("mouseout",function() {
+$('.lnb2').css("display","none")});
+$('.menu2').on("mouseout",function() {
+$('.lnb2').css("display","none")});
+
+
+$('.menu3').on("mouseover",function() {
+$('.lnb3').css("display", "block").css("background-color","rgba(0, 0, 255, 0.3)"); 
+});
+$('.lnb3').on("mouseover",function() {
+$('.lnb3').css("display","block")});
+$('.lnb3').on("mouseout",function() {
+$('.lnb3').css("display","none")});
+$('.menu3').on("mouseout",function() {
+$('.lnb3').css("display","none")});
+
+
+$('.menu4').on("mouseover",function() {
+$('.lnb4').css("display", "block").css("background-color","rgba(0, 0, 255, 0.3)"); 
+});
+$('.lnb4').on("mouseover",function() {
+$('.lnb4').css("display","block")});
+$('.lnb4').on("mouseout",function() {
+$('.lnb4').css("display","none")});
+$('.menu4').on("mouseout",function() {
+$('.lnb4').css("display","none")});
+
+
+$('.menu5').on("mouseover",function() {
+$('.lnb5').css("display", "block").css("background-color","rgba(0, 0, 255, 0.3)"); 
+});
+$('.lnb5').on("mouseover",function() {
+$('.lnb5').css("display","block")});
+$('.lnb5').on("mouseout",function() {
+$('.lnb5').css("display","none")});
+$('.menu5').on("mouseout",function() {
+$('.lnb5').css("display","none")});
+
+
+
+
