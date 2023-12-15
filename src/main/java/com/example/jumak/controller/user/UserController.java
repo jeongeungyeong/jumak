1cf59ff0101b2f3dacc0ddd2323f93f6bd4efdf9
seungbishin <tmdql1313@gmail.com>
2023-12-15 12:53:28 +0900

    html css thymeleaf

--- src/main/java/com/example/jumak/controller/user/UserController.java
+++ src/main/java/com/example/jumak/controller/user/UserController.java
@@ -1,4 +1,18 @@
 package com.example.jumak.controller.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/password-ok")
+    public String passwordOk(){
+        return "user/find_pw/complete_pwd";
+    }
+
 }
