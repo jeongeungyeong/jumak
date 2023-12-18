d136c9ce26854458ba274b686ee90fef66311702
refactoring.K <refactoring.k@gmail.com>
2023-12-18 13:30:14 +0900

    1차 수정중

---
+++ src/main/java/com/example/jumak/controller/main/Main.java
@@ -0,0 +1,12 @@
+package com.example.jumak.controller.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Main {
+    @GetMapping("/")
+    public String mainPage(){
+        return "main/main";
+    }
+}
--- src/main/java/com/example/jumak/mapper/user/UserMapper.java
+++ src/main/java/com/example/jumak/mapper/user/UserMapper.java
@@ -1,4 +1,8 @@
 package com.example.jumak.mapper.user;
 
+import com.example.jumak.domain.dto.user.UserDto;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface UserMapper {
 }
