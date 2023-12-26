7cba2c9da328fb6e826abb214f01a05944f596e1
croiss7 <jisu787@gmail.com>
2023-12-26 09:22:53 +0900

    join

--- src/main/java/com/example/jumak/controller/user/UserController.java
+++ src/main/java/com/example/jumak/controller/user/UserController.java
@@ -1,10 +1,15 @@
 package com.example.jumak.controller.user;
 
+import com.example.jumak.domain.dto.user.UserDto;
 import com.example.jumak.service.user.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
  private final UserService userService;
 
+// 단순페이지 이동 처리
     @GetMapping("/join/agree")
     public String joinAgree(){
         return "user/join/join";
@@ -36,8 +42,6 @@ public class UserController {
     public String findId(){
         return "user/find_id/find_id";
     }
-
-
 
     @GetMapping("/find-pwd")
     public String findPwd(){
@@ -69,6 +73,28 @@ public class UserController {
         return "user/find_pw/complete_pwd";
     }
 
+// 회원가입 처리
+    @PostMapping("/joinInfo")
+    public RedirectView join(UserDto userDto){
+        userService.register(userDto);
+        return new RedirectView("/user/login/login");
+    }
 
+// 로그인 처리
+    @PostMapping("/login")
+    public RedirectView login(String userId, String userPassword, HttpServletRequest req){
+        Long userNumber = userService.findUserNumber(userId, userPassword);
+        HttpSession session = req.getSession();
+        session.setAttribute("userNumber", userNumber);
+        return new RedirectView("/admin");  //메인화면경로 설정하기
+    }
 
-}
+// 로그아웃 처리
+    @GetMapping("/logout")
+    public RedirectView logout(HttpServletRequest req){
+        req.getSession().invalidate();
+        return new RedirectView("/admin"); //메인화면경로 설정하기
+    }
+
+    }
+
