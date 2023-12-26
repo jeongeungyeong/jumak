fdb7d6bd1115b21cda240ed5c37e89d83b567f6f
jeongeungyeong <jeongeung1124@gmail.com>
2023-12-26 12:59:55 +0900

    12/26 학원

--- src/main/java/com/example/jumak/mapper/product/ProductImgMapper.java
+++ src/main/java/com/example/jumak/mapper/product/ProductImgMapper.java
@@ -1,9 +1,19 @@
 package com.example.jumak.mapper.product;
 
+import com.example.jumak.domain.dto.product.ProductImgMainDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
 
 @Mapper
 public interface ProductImgMapper {
+//    상품 메인 이미지 등록
+    void insert(ProductImgMainDto productImgMainDto);
+
+//    상품 메인 이미지 조회
+    List<ProductImgMainDto> selectList(Long productNumber);
+
 //    상품 디테일 이미지
 
 }
