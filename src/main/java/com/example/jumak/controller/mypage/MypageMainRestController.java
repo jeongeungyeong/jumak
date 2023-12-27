0cdd630ace94fed9e3dcd9239280deaf33d38183
wlsql852 <108854643+wlsql852@users.noreply.github.com>
2023-12-27 12:51:41 +0900

    12-27 최종 업뎃

--- src/main/java/com/example/jumak/controller/mypage/MypageMainRestController.java
+++ src/main/java/com/example/jumak/controller/mypage/MypageMainRestController.java
@@ -1,12 +1,27 @@
 package com.example.jumak.controller.mypage;
 
+import com.example.jumak.domain.vo.myPage.OrderDetailVo;
+import com.example.jumak.service.mypage.SearchShippingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping()
+@RequestMapping("/mypage")
 public class MypageMainRestController {
+
+    private final SearchShippingService searchShippingService;
+
+    //    url로 데이터를 넘겨받아 조회가 가능하다.
+//    경로상의 데이터를 받기 위해서는 @PathVariable 을 사용한다.
+    @GetMapping("/search-shipping/{orderDate}")
+    public void periodInquiry(@RequestBody OrderDetailVo orderDetailVo) {
+        List<OrderDetailVo> orderDetailList = searchShippingService.findOrderDetail(orderDetailVo);
+    }
 
 }
