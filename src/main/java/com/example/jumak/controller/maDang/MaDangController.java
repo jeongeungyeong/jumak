8491d74ad332e3a0defa254525c8891a38574136
refactoringK <refactoring.k@gmail.com>
2023-12-28 12:39:29 +0900

    퍼블리싱 완료

--- src/main/java/com/example/jumak/controller/maDang/MaDangController.java
+++ src/main/java/com/example/jumak/controller/maDang/MaDangController.java
@@ -1,4 +1,40 @@
 package com.example.jumak.controller.maDang;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/madang")
 public class MaDangController {
+
+//    임시 이동 처리
+    @GetMapping("/list")
+    public String madangList(){
+        return "madang/madang-list";
+    }
+
+//    임시 이동 처리
+    @GetMapping("/detail")
+    public String madangDetail(){
+        return "madang/madang-detail";
+    }
+
+//    임시 이동 처리
+    @GetMapping("/write")
+    public String madangWrite(){
+        return "madang/madang-write";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
