ffdcda04dcc44b84ed62d9df68cd950afa7e8f7b
himawari00 <n9343226@gmail.com>
2024-01-05 13:02:36 +0900

    search 2023-01-05

--- src/main/java/com/example/jumak/controller/main/MainController.java
+++ src/main/java/com/example/jumak/controller/main/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -23,9 +24,9 @@ public class MainController {
 
     //    임시 이동경로
     @GetMapping("/search")
-    public String searchPage(String keyword, Model model){
+    public String searchPage(@RequestParam("keyword") String keyword, Model model){
         List<ProductSearchVo> searchList = mainService.findSearch(keyword);
-        model.addAttribute(searchList);
+        model.addAttribute("searchList",searchList);
         model.addAttribute("keyword",keyword);
         return "product/search";
     }
