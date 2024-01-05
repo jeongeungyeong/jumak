f4c8046c9d0011e708e14ab1780bf31a04e7e70c
jeongeungyeong <jeongeung1124@gmail.com>
2024-01-05 11:03:52 +0900

    order 페이지

--- src/main/java/com/example/jumak/controller/order/OrderController.java
+++ src/main/java/com/example/jumak/controller/order/OrderController.java
@@ -3,6 +3,7 @@ package com.example.jumak.controller.order;
 import com.example.jumak.domain.dto.order.OrderDto;
 import com.example.jumak.domain.dto.product.ProductDto;
 import com.example.jumak.domain.vo.order.OrderFinishVo;
+import com.example.jumak.domain.vo.product.ProductDetailVo;
 import com.example.jumak.service.order.OrderService;
 import com.example.jumak.service.product.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -22,22 +23,25 @@ public class OrderController {
 
 
     @GetMapping("/next")
-    public String orderMain(Long productNumber,
+    public String orderMain(@RequestParam("productNumber") Long productNumber,
                             @RequestParam("productCount") Long productCount,
                             Model model){
 // productNumber 로 next페이지에 넘겨줄 정보 서비스에서 뽑아서 넘겨주고
 //        productCount 는 그냥 넘겨주자
-        ProductDto productDetail = productService.findByNumber(productNumber);
-
+        ProductDetailVo productDetail = productService.findByDNumber(productNumber);
 
         model.addAttribute("productDetail",productDetail);
         model.addAttribute("productCount",productCount);
-        // productCount는 @{productCount}이케만 html에 쓰면댐
+//        상품 토탈 가격
+        model.addAttribute("totalPrice", productCount * productDetail.getProductPrice());
+//        페이먼트 토탈 가격
+        model.addAttribute("paymentTotal", (productCount *(productDetail.getProductPrice() - (productDetail.getProductPrice() *(productDetail.getProductDiscount()/100.0))))
+                +3000);
+        // productCount는 ${productCount}로 html에 사용
 
-        //이제 화면아래 자기정보뿌려야하잖아? 그거이제매퍼에서부터 찾아서
+        //이제 화면아래 자기정보뿌리기 그거이제매퍼에서부터 찾아서
 
-//여기다가 또 서비스연결해서 모델에뿌리면댐!!
-        //완전 나이스...이거 나오는지 함만 볼게!
+//여기다가 또 서비스연결해서 모델로 뿌리기
         return "order/storeorder";
     }
 
