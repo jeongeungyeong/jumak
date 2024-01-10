4476fdaba3db0a4c30d6158992998601f34f1b54
himawari00 <n9343226@gmail.com>
2024-01-10 12:38:05 +0900

    work/find

--- src/main/java/com/example/jumak/mapper/main/MainMapper.java
+++ src/main/java/com/example/jumak/mapper/main/MainMapper.java
@@ -1,9 +1,13 @@
 package com.example.jumak.mapper.main;
 
+import com.example.jumak.domain.dto.product.ProductDto;
 import com.example.jumak.domain.dto.user.UserDto;
 import com.example.jumak.domain.vo.main.ProductSearchVo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 
 @Mapper
@@ -13,4 +17,12 @@ public interface MainMapper {
 
 // 상품 검색 카운트
     public int countSearch(String keyword);
+
+// 나만의 술찾기
+//    public List<ProductDto> selectTag(@Param("tag1") Long tag1,@Param("tag2") Long tag2
+//            ,@Param("tag3") Long tag3);
+
+    public List<ProductDto> selectTag(@Param("arr") Integer[] arr,@Param("count") int count,
+                                      @Param("productCategoryNumber1") Long productCategoryNumber1,
+                                      @Param("productCategoryNumber2") Long productCategoryNumber2);
 }
